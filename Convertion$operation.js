ea164df52dc30cebca461e00b6af6ce2e04cb8aa
Atanu <atanu.2016ghosh@gmail.com>
2025-02-09 13:31:05 +0530

    addednew data

--- Convertion$operation.js
+++ Convertion$operation.js
@@ -1,7 +1,21 @@
 // Convertion and Operation in javasctipr
 
 //Number
-let score = 33
+let score = "33abc"
 
-console.log (score);
+// console.log (typeof score);
+// console.log (typeof (score));
 
+let valueInNumber = Number(score)
+console.log (typeof valueInNumber); 
+
+
+/* NOTES ************************************************
+
+1 => true ; 0 =>  false
+"" => false ; "Data" => true
+undefined => Number = NaN
+string  => number = Nan
+null => number = 0
+
+*/
